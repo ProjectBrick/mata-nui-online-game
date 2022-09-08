c182380f9ba806081234ada785e636b3aee50266
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2022-09-07 23:54:00 -0400

    Dynamic tokens in docs

--- util/doc.mjs
+++ util/doc.mjs
@@ -1,33 +1,56 @@
-import {readdir, readFile, writeFile} from 'fs/promises';
+import {readdir, readFile, stat, writeFile} from 'fs/promises';
+import {pathToFileURL} from 'url';
 
 import {marked} from 'marked';
 
 import {templateStrings} from './string.mjs';
 
-export async function docs(src, dst, replace = []) {
+async function readTokens(file) {
+	const u = pathToFileURL(file);
+	try {
+		await stat(u);
+	}
+	catch (err) {
+		if (err.code === 'ENOENT') {
+			return [];
+		}
+		throw err;
+	}
+	return (await import(u)).default;
+}
+
+function firstHeader(body) {
+	const m = body.match(/<h\d[^>]*>([\s\S]*?)<\/h\d>/);
+	if (!m || !m[0]) {
+		throw new Error('No title');
+	}
+	return m[0];
+}
+
+export async function docs(src, dst) {
 	const template = await readFile(`${src}/template.html`, 'utf8');
+	const tokens = await readTokens(`${src}/tokens.mjs`);
+	const options = {
+		gfm: true,
+		breaks: true,
+		smartypants: true
+	};
 	await Promise.all(
 		(await readdir(src))
 			.filter(f => /^[^\.].*\.md$/i.test(f))
+			.sort()
 			.map(f => readFile(`${src}/${f}`, 'utf8').then(md => {
-				const body = marked(md, {
-					gfm: true,
-					breaks: true,
-					smartypants: true
-				}).trim();
-				const title = (
-					body.match(/<h\d[^>]*>([\s\S]*?)<\/h\d>/) || []
-				)[1] || '';
-				let html = templateStrings(template, {
-					title,
-					body
-				});
-				for (const [f, r] of replace) {
-					html = html.replace(f, r);
-				}
+				const body = marked(md, options).trim();
+				const title = firstHeader(body);
 				return writeFile(
 					`${dst}/${f}`.replace(/\.md$/i, '.html'),
-					html
+					tokens.reduce(
+						(html, [f, r]) => html.replaceAll(f, r),
+						templateStrings(template, {
+							title,
+							body
+						})
+					)
 				);
 			}))
 	);
