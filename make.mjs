22b10d581cd607df8d8858efdc9b10e763efb826
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2022-09-11 19:53:47 -0400

    Procedural tasks, release projectors

--- make.mjs
+++ make.mjs
@@ -130,108 +130,21 @@ task['clean'] = async () => {
 };
 
 task['build:pages'] = async () => {
-	const dest = 'build/pages';
-	await remove(dest);
-	await browser(dest);
-	await docs('docs', dest);
+	const build = 'build/pages';
+	await remove(build);
+	await browser(build);
+	await docs('docs', build);
 };
 
 task['build:browser'] = async () => {
-	const dest = 'build/browser';
-	await remove(dest);
-	await browser(`${dest}/data`);
+	const build = 'build/browser';
+	await remove(build);
+	await browser(`${build}/data`);
 	await outputFile(
-		`${dest}/${appFile}.html`,
+		`${build}/${appFile}.html`,
 		'<meta http-equiv="refresh" content="0;url=data/index.html">\n'
 	);
-	await docs('docs', dest);
-};
-
-task['build:windows'] = async () => {
-	const dest = 'build/windows';
-	await remove(dest);
-	const file = `${appFile}.exe`;
-	const b = new BundleWindows32(`${dest}/${file}`);
-	b.projector.versionStrings = {
-		FileVersion: version,
-		ProductVersion: versionShort,
-		CompanyName: author,
-		FileDescription: appName,
-		LegalCopyright: copyright,
-		ProductName: appName,
-		LegalTrademarks: '',
-		OriginalFilename: file,
-		InternalName: appFile,
-		Comments: ''
-	};
-	b.projector.iconFile = 'res/app-icon-windows.ico';
-	b.projector.patchWindowTitle = appName;
-	b.projector.removeCodeSignature = true;
-	await bundle(b, 'flash-player-32.0.0.465-windows-sa-debug');
-	await docs('docs', dest);
-};
-
-task['build:mac'] = async () => {
-	// Release projectors on Mac have slow performance when resized larger.
-	// Debug projectors do not have this performance issue.
-	const dest = 'build/mac';
-	await remove(dest);
-	const pkgInfo = 'APPL????';
-	const b = new BundleMacApp(`${dest}/${appFile}.app`);
-	b.projector.binaryName = appFile;
-	b.projector.pkgInfoData = pkgInfo;
-	b.projector.infoPlistData = (new Plist(new ValueDict(new Map([
-		['CFBundleInfoDictionaryVersion', new ValueString('6.0')],
-		['CFBundleDevelopmentRegion', new ValueString('en-US')],
-		['CFBundleExecutable', new ValueString('')],
-		['CFBundleIconFile', new ValueString('')],
-		['CFBundleName', new ValueString(appName)],
-		['NSHumanReadableCopyright', new ValueString(copyright)],
-		['CFBundleGetInfoString', new ValueString(copyright)],
-		['CFBundleIdentifier', new ValueString(appDomain)],
-		['CFBundleVersion', new ValueString(version)],
-		['CFBundleLongVersionString', new ValueString(version)],
-		['CFBundleShortVersionString', new ValueString(versionShort)],
-		['CFBundlePackageType', new ValueString(pkgInfo.substring(0, 4))],
-		['CFBundleSignature', new ValueString(pkgInfo.substring(4))],
-		['NSAppTransportSecurity', new ValueDict(new Map([
-			['NSAllowsArbitraryLoads', new ValueBoolean(true)]
-		]))],
-		['NSSupportsAutomaticGraphicsSwitching', new ValueBoolean(true)],
-		['NSHighResolutionCapable', new ValueBoolean(true)],
-		['CSResourcesFileMapped', new ValueBoolean(true)],
-		['LSPrefersCarbon', new ValueString('YES')],
-		['NSAppleScriptEnabled', new ValueString('YES')],
-		['NSMainNibFile', new ValueString('MainMenu')],
-		['NSPrincipalClass', new ValueString('NSApplication')]
-	])))).toXml();
-	b.projector.iconFile = 'res/app-icon-mac.icns';
-	b.projector.patchWindowTitle = appName;
-	b.projector.removeInfoPlistStrings = true;
-	b.projector.removeCodeSignature = true;
-	await bundle(b, 'flash-player-32.0.0.465-mac-sa-debug-zip');
-	await docs('docs', dest);
-};
-
-task['build:linux-i386'] = async () => {
-	const dest = 'build/linux-i386';
-	await remove(dest);
-	const b = new BundleLinux32(`${dest}/${appFile}`);
-	b.projector.patchProjectorPath = true;
-	b.projector.patchWindowTitle = appName;
-	await bundle(b, 'flash-player-11.2.202.644-linux-i386-sa-debug', true);
-	await docs('docs', dest);
-};
-
-task['build:linux-x86_64'] = async () => {
-	const dest = 'build/linux-x86_64';
-	await remove(dest);
-	const b = new BundleLinux64(`${dest}/${appFile}`);
-	b.projector.patchProjectorPath = true;
-	b.projector.patchProjectorOffset = true;
-	b.projector.patchWindowTitle = appName;
-	await bundle(b, 'flash-player-32.0.0.465-linux-x86_64-sa-debug', true);
-	await docs('docs', dest);
+	await docs('docs', build);
 };
 
 task['dist:browser:zip'] = async () => {
@@ -242,57 +155,144 @@ task['dist:browser:tgz'] = async () => {
 	await makeTgz(`dist/${distName}-Browser.tgz`, 'build/browser');
 };
 
-task['dist:windows:zip'] = async () => {
-	await makeZip(`dist/${distName}-Windows.zip`, 'build/windows');
-};
+for (const [type, pkg] of [
+	['i386', 'flash-player-32.0.0.465-windows-sa'],
+	['i386-debug', 'flash-player-32.0.0.465-windows-sa-debug']
+]) {
+	const build = `build/windows-${type}`;
+	task[`build:windows-${type}`] = async () => {
+		await remove(build);
+		const file = `${appFile}.exe`;
+		const b = new BundleWindows32(`${build}/${file}`);
+		b.projector.versionStrings = {
+			FileVersion: version,
+			ProductVersion: versionShort,
+			CompanyName: author,
+			FileDescription: appName,
+			LegalCopyright: copyright,
+			ProductName: appName,
+			LegalTrademarks: '',
+			OriginalFilename: file,
+			InternalName: appFile,
+			Comments: ''
+		};
+		b.projector.iconFile = 'res/app-icon-windows.ico';
+		b.projector.patchWindowTitle = appName;
+		b.projector.removeCodeSignature = true;
+		await bundle(b, pkg);
+		await docs('docs', build);
+	};
+	task[`dist:windows-${type}:zip`] = async () => {
+		await makeZip(`dist/${distName}-Windows-${type}.zip`, build);
+	};
+	task[`dist:windows-${type}:exe`] = async () => {
+		await makeExe(
+			`dist/${distName}-Windows.exe`,
+			appDomain,
+			appName,
+			appFile,
+			version,
+			author,
+			copyright,
+			'LICENSE.txt',
+			'res/inno-icon.ico',
+			'res/inno-header/*.bmp',
+			'res/inno-sidebar/*.bmp',
+			`${build}/*`,
+			[
+				[`${appFile}.exe`, appFile, true, true],
+				['README.html', `${appFile} - README`]
+			]
+		);
+	};
+}
 
-task['dist:windows:exe'] = async () => {
-	await makeExe(
-		`dist/${distName}-Windows.exe`,
-		appDomain,
-		appName,
-		appFile,
-		version,
-		author,
-		copyright,
-		'LICENSE.txt',
-		'res/inno-icon.ico',
-		'res/inno-header/*.bmp',
-		'res/inno-sidebar/*.bmp',
-		'build/windows/*',
-		[
-			[`${appFile}.exe`, appFile, true, true],
-			['README.html', `${appFile} - README`]
-		]
-	);
-};
+for (const [type, pkg] of [
+	['x86_64', 'flash-player-32.0.0.465-mac-sa-zip'],
+	['x86_64-debug', 'flash-player-32.0.0.465-mac-sa-debug-zip']
+]) {
+	const build = `build/mac-${type}`;
+	task[`build:mac-${type}`] = async () => {
+		await remove(build);
+		const pkgInfo = 'APPL????';
+		const b = new BundleMacApp(`${build}/${appFile}.app`);
+		b.projector.binaryName = appFile;
+		b.projector.pkgInfoData = pkgInfo;
+		b.projector.infoPlistData = (new Plist(new ValueDict(new Map([
+			['CFBundleInfoDictionaryVersion', new ValueString('6.0')],
+			['CFBundleDevelopmentRegion', new ValueString('en-US')],
+			['CFBundleExecutable', new ValueString('')],
+			['CFBundleIconFile', new ValueString('')],
+			['CFBundleName', new ValueString(appName)],
+			['NSHumanReadableCopyright', new ValueString(copyright)],
+			['CFBundleGetInfoString', new ValueString(copyright)],
+			['CFBundleIdentifier', new ValueString(appDomain)],
+			['CFBundleVersion', new ValueString(version)],
+			['CFBundleLongVersionString', new ValueString(version)],
+			['CFBundleShortVersionString', new ValueString(versionShort)],
+			['CFBundlePackageType', new ValueString(pkgInfo.substring(0, 4))],
+			['CFBundleSignature', new ValueString(pkgInfo.substring(4))],
+			['NSAppTransportSecurity', new ValueDict(new Map([
+				['NSAllowsArbitraryLoads', new ValueBoolean(true)]
+			]))],
+			['NSSupportsAutomaticGraphicsSwitching', new ValueBoolean(true)],
+			['NSHighResolutionCapable', new ValueBoolean(true)],
+			['CSResourcesFileMapped', new ValueBoolean(true)],
+			['LSPrefersCarbon', new ValueString('YES')],
+			['NSAppleScriptEnabled', new ValueString('YES')],
+			['NSMainNibFile', new ValueString('MainMenu')],
+			['NSPrincipalClass', new ValueString('NSApplication')]
+		])))).toXml();
+		b.projector.iconFile = 'res/app-icon-mac.icns';
+		b.projector.patchWindowTitle = appName;
+		b.projector.removeInfoPlistStrings = true;
+		b.projector.removeCodeSignature = true;
+		await bundle(b, pkg);
+		await docs('docs', build);
+	};
+	task[`dist:mac-${type}:tgz`] = async () => {
+		await makeTgz(`dist/${distName}-Mac-${type}.tgz`, build);
+	};
+	task[`dist:mac-${type}:dmg`] = async () => {
+		await makeDmg(
+			`dist/${distName}-Mac-${type}.dmg`,
+			appDmgTitle,
+			'res/dmg-icon.icns',
+			'res/dmg-background/dmg-background.png',
+			[640, 512],
+			128,
+			[
+				[-160, -148, 'file', `${build}/${appFile}.app`],
+				[160, -148, 'link', '/Applications'],
+				[0, 100, 'file', `${build}/README.html`]
+			]
+		);
+	};
+}
 
-task['dist:mac:tgz'] = async () => {
-	await makeTgz(`dist/${distName}-Mac.tgz`, 'build/mac');
-};
-
-task['dist:mac:dmg'] = async () => {
-	await makeDmg(
-		`dist/${distName}-Mac.dmg`,
-		appDmgTitle,
-		'res/dmg-icon.icns',
-		'res/dmg-background/dmg-background.png',
-		[640, 512],
-		128,
-		[
-			[-160, -148, 'file', `build/mac/${appFile}.app`],
-			[160, -148, 'link', '/Applications'],
-			[0, 100, 'file', 'build/mac/README.html']
-		]
-	);
-};
-
-task['dist:linux-i386:tgz'] = async () => {
-	await makeTgz(`dist/${distName}-Linux-i386.tgz`, 'build/linux-i386');
-};
-
-task['dist:linux-x86_64:tgz'] = async () => {
-	await makeTgz(`dist/${distName}-Linux-x86_64.tgz`, 'build/linux-x86_64');
-};
+for (const [type, pkg] of [
+	['i386', 'flash-player-11.2.202.644-linux-i386-sa'],
+	['i386-debug', 'flash-player-11.2.202.644-linux-i386-sa-debug'],
+	['x86_64', 'flash-player-32.0.0.465-linux-x86_64-sa'],
+	['x86_64-debug', 'flash-player-32.0.0.465-linux-x86_64-sa-debug']
+]) {
+	const build = `build/linux-${type}`;
+	task[`build:linux-${type}`] = async () => {
+		await remove(build);
+		const b = new (/x86_64/.test(type) ? BundleLinux64 : BundleLinux32)(
+			`${build}/${appFile}`
+		);
+		if (b instanceof BundleLinux64) {
+			b.projector.patchProjectorOffset = true;
+		}
+		b.projector.patchProjectorPath = true;
+		b.projector.patchWindowTitle = appName;
+		await bundle(b, pkg, true);
+		await docs('docs', build);
+	};
+	task[`dist:linux-${type}:tgz`] = async () => {
+		await makeTgz(`dist/${distName}-Linux-${type}.tgz`, build);
+	};
+}
 
 process.exitCode = await task[process.argv[2] || '']();
